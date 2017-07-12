cdcff32747a8600b9a9dc13ced71fb21bb1e09d3
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-12 16:31:53 -0400

    added two redirects and one page(favourites)

--- server.rb
+++ server.rb
@@ -1,8 +1,16 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
 end
+
+get '/gallery' do
+  redirect to('/portfolio')
+end 
 
 get '/portfolio' do
   erb :gallery
@@ -12,4 +20,9 @@ get '/about_me' do
   @skills = ['git', 'HTML', 'CSS', 'Ruby']
   @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['https://www.facebook.com/', 'https://alexa.bitmaker.co/wdi/june-2017/assignments/2482']
+  erb :favourites
 end
